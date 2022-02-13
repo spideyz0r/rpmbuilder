2624244bcb769b59c8e48d4e61532ca74c339b74
Breno Brand Fernandes <brandfbb@gmail.com>
2022-02-13 15:13:12 -0500

    Testing another package: redis

--- handler.sh
+++ handler.sh
@@ -5,23 +5,23 @@ packages=$(git diff main... --name-only | grep spec$ | cut -d / -f2)
 function build()
 {
 	pkg=$1
-	release=$(grep Release packages/$1/$1.spec | cut -d ":" -f2  | tr -d " " | cut -b -1)
-	version=$(grep Version packages/$1/$1.spec | cut -d ":" -f2  | tr -d " ")
+	release=$(grep 'Release:' packages/$1/$1.spec | cut -d ":" -f2  | tr -d " " | cut -b -1)
+	version=$(grep 'Version:' packages/$1/$1.spec | cut -d ":" -f2  | tr -d " ")
 	name="${pkg}-${version}-${release}.fc34.src.rpm"
 	echo "###################################################"
 	echo "Building $pkg => $name. Logs in $name.log"
-	time docker run -it -d --name ${pkg}-build -v /home/circleci/project:/project --cap-add=SYS_ADMIN --security-opt apparmor:unconfined mockzor &>$pkg.log
+	docker run -it -d --name ${pkg}-build -v /home/circleci/project:/project --cap-add=SYS_ADMIN --security-opt apparmor:unconfined mockzor
 	echo "Building SRPM for $pkg"
-	time docker exec ${pkg}-build /project/scripts/buildsrpm.sh $pkg &>>$pkg.log
+	docker exec ${pkg}-build /project/scripts/buildsrpm.sh $pkg
 	echo "Running mock for $name"
-	time docker exec ${pkg}-build /project/scripts/runmock.sh rocky+epel-8-x86_64 $name &>>$pkg.log || echo "Build for $pkg has failed!"
+	docker exec ${pkg}-build /project/scripts/runmock.sh rocky+epel-8-x86_64 $name
 	echo "Copying $pkg RPMs"
-	time docker exec ${pkg}-build /project/scripts/copyrpms.sh rocky+epel-8-x86_64 $name 2>&1 | tee $pkg.log
+	docker exec ${pkg}-build /project/scripts/copyrpms.sh rocky+epel-8-x86_64 $name
 }
 
 for pkg in $packages
 do
-	build $pkg&
+	build $pkg
 done
 wait
 echo "All RPMs were built with success!"
--- scripts/copyrpms.sh
+++ scripts/copyrpms.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir /project/files/$2
+mkdir -p /project/files/$2
 cp -v /var/lib/mock/$1/result/* /project/files/$2
